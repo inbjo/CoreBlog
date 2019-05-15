2c84236409d40d220666dba7273be1c7394acfc7
Flex <2345@mail.com>
2019-05-15 17:30:59 +0800

    增加xss防御

--- app/Observers/PostObserver.php
+++ app/Observers/PostObserver.php
@@ -8,6 +8,7 @@ class PostObserver
 {
     public function saving(Post $post)
     {
+        $post->content= clean($post->content, 'user_post_content');
         $post->description = make_description($post->content);
     }
 }
