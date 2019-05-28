f596ee273f63f3a0bebab5a172ab57a76dde003f
Flex <2345@mail.com>
2019-05-28 17:22:55 +0800

    增加系统设置页面

---
+++ app/Http/Controllers/SettingsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SettingsController extends Controller
+{
+
+    public function index()
+    {
+        return view('settings.index');
+    }
+}
