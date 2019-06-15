7afd6d9e75f540287f926786a7aa8af57a3c7777
Flex <2345@mail.com>
2019-06-15 23:01:58 +0800

    finish alipay support

--- app/Http/Controllers/Pay/AlipayController.php
+++ app/Http/Controllers/Pay/AlipayController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers\Pay;
 
 use App\Models\Order;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Log;
 use Yansongda\Pay\Pay;
 
 class AlipayController extends Controller
