6eebf1dec6b931285e092b38b6646ae9a4810bc7
Flex <2345@mail.com>
2019-06-21 11:37:44 +0800

    fix favorite self bug

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -17,13 +17,19 @@ class FavoritesController extends Controller
 
     public function comment(Comment $comment)
     {
+        if ($comment->user_id == auth()->id()) {
+            return ['code' => 1, 'msg' => '不能给自己点赞哦'];
+        }
         $comment->user->notify(new CommentWereFavorited($comment));
         return $comment->favorite();
     }
 
     public function post($id)
     {
-        $post= Post::findOrFail($id);
+        $post = Post::findOrFail($id);
+        if ($post->user_id == auth()->id()) {
+            return ['code' => 1, 'msg' => '不能给自己点赞哦'];
+        }
         $post->user->notify(new PostWereFavorited($post));
         return $post->favorite();
     }
