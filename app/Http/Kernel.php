4f3d7b62db0b10eae000fbb2ecd11e5e768c66ef
Flex <2345@mail.com>
2019-08-08 10:21:19 +0800

    add api cors support

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            'cors'
         ],
     ];
 
@@ -61,6 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
