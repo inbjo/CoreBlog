1e3b332bd6a5d8a83caa9726ea1ca95896c8cf7b
Flex <2345@mail.com>
2019-08-13 15:45:57 +0800

    remove jwt package

--- routes/api.php
+++ routes/api.php
@@ -13,16 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
-    Route::post('login', 'AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
-});
-
 Route::get('/site', 'Api\SettingsController@index');
 Route::resource('/category', 'Api\CategoriesController');
 Route::resource('/comment', 'Api\CommentsController');
